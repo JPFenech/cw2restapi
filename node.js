004e6631884ce7abbd078a109fca8218d7f6c2cb
Jean <fenechjeanpierre@gmail.com>
2021-01-24 12:38:02 +0100

    node file

---
+++ node.js
@@ -0,0 +1,89 @@
+// load Express.js
+const express = require('express')
+const app = express()
+
+// parse the request parameters
+app.use(express.json())
+
+
+// connect to MongoDB
+const MongoClient = require('mongodb').MongoClient;let db;
+MongoClient.connect('mongodb+srv://fenechjeanpierre:P@ssWORD1@cluster0.xfu9d.mongodb.net/Testing?retryWrites=true&w=majority', (err, client) => {
+db = client.db('Testing')
+    })
+
+// get the collection name
+        app.param('collectionName', (req, res, next, collectionName) => {
+            req.collection = db.collection(collectionName)
+// console.log('collection name:', req.collection)
+                return next()
+    })
+
+// dispaly a message for root path to show that API is working
+    app.get('/', function (req, res) {res.send('Select a collection, e.g., /collection/lessons')
+    })
+
+// retrieve all the objects from an collection
+        app.get('/collection/:collectionName', (req, res) => {req.collection.find({}).toArray((e, results) => {
+            if (e) return next(e)
+                res.send(results)
+    })
+
+})
+
+//retrieve an object by Mogodb ID
+const ObjectID = require('mongodb').ObjectID;
+app.get('/collection/:collectionName/:id', (req, res, next)  =>  {
+    req.collection.findOne (
+        { _id: new ObjectID(req.params.id)  },
+        (e, result)  => {
+            if (e) return next(e)
+            res.send(result)
+        })
+})
+
+ //add an object
+        app.post('/collection/:collectionName', (req, res, next) =>  {
+                    req.collection.insert(req.body,  (e, results)  =>  {
+                        if (e) return(e)
+                            res.send(results.ops)
+            })
+ })
+
+//  fetch('http://localhost:3210').then(
+//     function (response) {
+//     response.json().then(
+//     function (json) {
+//     // save the returned JSON object to 'products'
+//     // note that we used 'store.products' instead of 'this.products'
+//     store.products = json;
+//     })})
+
+
+
+
+//  // update an object by ID
+//         app.put('/collection/:collectionName/:id', (req, res, next) => {
+//                 req.collection.update(
+//                     { _id: new ObjectID(req.params.id) },
+//                     { $set: req.body },
+//                     { safe: true, multi: false },(e, result) => {
+//                     if (e) return next(e)
+//                                 res.send((result.result.n === 1) ?
+//                                     {msg: 'success'} : { msg: 'error'})
+//                         })
+// })
+
+// // delete an object by ID
+//     app.delete('/collection/:collectionName/:id', (req, res, next) => {
+//             req.collection.deleteOne(
+//                 { _id: ObjectID(req.params.id) },(e, result) => {
+//             if (e) return next(e)
+//                 res.send((result.result.n === 1) ?
+//                     {msg: 'success'} : {msg: 'error'})
+//             })
+// })
+
+app.listen(3210, function(){
+    console.log("running port 3210");
+});
